d16323d85a9f056a493cb3a13b844240e284ccbd
Breno Viana <brenomfviana@gmail.com>
2023-10-05 14:58:02 -0300

    refatora e adiciona testes

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub mod docs;
 
 #[cfg(test)]
-mod tests {
+mod cpf_tests {
     use super::*;
     use crate::docs::doc::Document;
 
@@ -19,7 +19,7 @@ mod tests {
 
     #[test]
     fn validate_cpf_3() {
-        let doc: Document = Document::new("111.111.111-11");
+        let doc: Document = Document::new("63929247011");
         assert_eq!(true, docs::cpf::validate(doc, true));
     }
 
@@ -27,5 +27,23 @@ mod tests {
     fn validate_cpf_4() {
         let doc: Document = Document::new("639.292.470-11");
         assert_eq!(true, docs::cpf::validate(doc, false));
+    }
+}
+
+#[cfg(test)]
+mod cnpj_tests {
+    use super::*;
+    use crate::docs::doc::Document;
+
+    #[test]
+    fn validate_cnpj_1() {
+        let doc: Document = Document::new("05200851000100");
+        assert_eq!(true, docs::cnpj::validate(doc));
+    }
+
+    #[test]
+    fn validate_cnpj_2() {
+        let doc: Document = Document::new("05.200.851/0001-00");
+        assert_eq!(true, docs::cnpj::validate(doc));
     }
 }
