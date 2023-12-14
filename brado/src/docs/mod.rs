38437a8d77670c02ec8cefab11360a1388379f93
Breno Viana <brenomfviana@gmail.com>
2023-12-14 15:53:50 -0300

    fixes imports

--- brado/src/docs/mod.rs
+++ brado/src/docs/mod.rs
@@ -2,4 +2,3 @@ pub mod cnpj;
 pub mod common;
 pub mod cpf;
 pub mod doc;
-pub use doc::Document;
--- brado/src/lib.rs
+++ brado/src/lib.rs
@@ -1,4 +1,15 @@
+//! # brado documentation
+//! 
+//! brado is a brazilian docs validator.
+//! 
+//! ## Example
+//! ```
+//! use brado;
+//! use brado::Document;
+//! let cpf_doc: Document = Document::new("639.292.470-11");
+//! brado::cpf::validate(&cpf_doc, true, false);
+//! ```
 mod docs;
 pub use crate::docs::cnpj;
 pub use crate::docs::cpf;
-pub use crate::docs::Document;
+pub use crate::docs::doc::Document;
