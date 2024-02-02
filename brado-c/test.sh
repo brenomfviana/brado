dfe373c48a2bed67b6818008a7ed25d0eed9b113
Breno Viana <brenomfviana@gmail.com>
2024-02-02 15:35:48 -0300

    fmt
    Co-authored-by: PedroRegisPOAR <pedroalencarregis@hotmail.com>

--- brado-c/test.sh
+++ brado-c/test.sh
@@ -4,7 +4,7 @@ cargo build
 
 export LD_LIBRARY_PATH="$(pwd)"/target/debug:$LD_LIBRARY_PATH
 
-cbindgen --config cbindgen.toml --crate brado-c  --output include/brado.h
+cbindgen --config cbindgen.toml --crate brado-c --output include/brado.h
 
 g++ --std=c++11 -o target/test examples/test.cpp -Ltarget/debug/ -lbrado
 
