a61a2f1f2a316ea1d897b5c5489ee128f2712bfb
Breno Viana <brenomfviana@gmail.com>
2024-02-03 18:03:05 -0300

    refatora testes

--- brado/tests/cnpj.rs
+++ brado/tests/cnpj.rs
@@ -4,69 +4,69 @@ mod cnpj_tests {
 
     #[test]
     fn cnpj_validate_1_valid_bare_cnpj() {
-        let bare_cnpj: String = String::from("05200851000100");
-        assert_eq!(true, brado::cnpj::validate(&bare_cnpj));
+        let bare_cnpj: &str = "05200851000100";
+        assert_eq!(true, brado::cnpj::validate(bare_cnpj));
     }
 
     #[test]
     fn cnpj_validate_2_valid_masked_cnpj() {
-        let masked_cnpj: String = String::from("05.200.851/0001-00");
-        assert_eq!(true, brado::cnpj::validate(&masked_cnpj));
+        let masked_cnpj: &str = "05.200.851/0001-00";
+        assert_eq!(true, brado::cnpj::validate(masked_cnpj));
     }
 
     #[test]
     fn cnpj_validate_3_invalid_mask() {
-        let document = String::from("0.520.085/1000-100");
-        assert_eq!(brado::cnpj::validate(&document), false);
+        let document: &str = "0.520.085/1000-100";
+        assert_eq!(brado::cnpj::validate(document), false);
     }
 
     #[test]
     fn cnpj_validate_4_invalid_other_document_1() {
-        let document = String::from("052008510001");
-        assert_eq!(brado::cnpj::validate(&document), false);
+        let document: &str = "052008510001";
+        assert_eq!(brado::cnpj::validate(document), false);
     }
 
     #[test]
     fn cnpj_validate_5_invalid_other_document_2() {
-        let document = String::from("00.520.085/1000-100");
-        assert_eq!(brado::cnpj::validate(&document), false);
+        let document: &str = "00.520.085/1000-100";
+        assert_eq!(brado::cnpj::validate(document), false);
     }
 
     #[test]
     fn cnpj_validate_6_invalid_other_document_3() {
-        let document = String::from("05.200.851/0001-0:0");
-        assert_eq!(brado::cnpj::validate(&document), false);
+        let document: &str = "05.200.851/0001-0:0";
+        assert_eq!(brado::cnpj::validate(document), false);
     }
 
     #[test]
     fn cnpj_is_bare_1_bare_cnpj() {
-        let bare_cnpj = String::from("05200851000100");
-        assert_eq!(brado::cnpj::is_bare(&bare_cnpj), true);
+        let bare_cnpj: &str = "05200851000100";
+        assert_eq!(brado::cnpj::is_bare(bare_cnpj), true);
     }
 
     #[test]
     fn cnpj_is_bare_2_masked_cnpj() {
-        let masked_cnpj = String::from("05.200.851/0001-00");
-        assert_eq!(brado::cnpj::is_bare(&masked_cnpj), false);
+        let masked_cnpj: &str = "05.200.851/0001-00";
+        assert_eq!(brado::cnpj::is_bare(masked_cnpj), false);
     }
 
     #[test]
     fn cnpj_is_masked_1_masked_cnpj() {
-        let masked_cnpj = String::from("05.200.851/0001-00");
-        assert_eq!(brado::cnpj::is_masked(&masked_cnpj), true);
+        let masked_cnpj: &str = "05.200.851/0001-00";
+        assert_eq!(brado::cnpj::is_masked(masked_cnpj), true);
     }
 
     #[test]
     fn cnpj_is_masked_2_bare_cnpj() {
-        let bare_cnpj = String::from("05200851000100");
-        assert_eq!(brado::cnpj::is_masked(&bare_cnpj), false);
+        let bare_cnpj: &str = "05200851000100";
+        assert_eq!(brado::cnpj::is_masked(bare_cnpj), false);
     }
 
     #[test]
     fn cnpj_mask_1_bare_cnpj() {
-        let bare_cnpj = String::from("05200851000100");
+        let bare_cnpj: &str = "05200851000100";
         assert_eq!(
-            brado::cnpj::mask(&bare_cnpj),
+            brado::cnpj::mask(bare_cnpj),
             String::from("05.200.851/0001-00"),
         );
     }
@@ -74,14 +74,14 @@ mod cnpj_tests {
     #[test]
     #[should_panic(expected = "The given string cannot be masked as CNPJ")]
     fn cnpj_mask_2_masked_cnpj() {
-        let masked_cnpj = String::from("05.200.851/0001-00");
-        brado::cnpj::mask(&masked_cnpj);
+        let masked_cnpj: &str = "05.200.851/0001-00";
+        brado::cnpj::mask(masked_cnpj);
     }
 
     #[test]
     #[should_panic(expected = "The given string cannot be masked as CNPJ")]
     fn cnpj_mask_3_invalid_cnpj() {
-        let document = String::from("052008510001");
-        brado::cnpj::mask(&document);
+        let document: &str = "052008510001";
+        brado::cnpj::mask(document);
     }
 }
