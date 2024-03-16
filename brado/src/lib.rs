359d3608f68fd2ee5ea4f5d80ee04977fca5df59
Breno Viana <brenomfviana@gmail.com>
2024-03-16 14:07:01 -0300

    adiciona documentação geral da lib

--- brado/src/lib.rs
+++ brado/src/lib.rs
@@ -1,11 +1,25 @@
-//! # brado documentation
+// Copyright 2024 Developers of the Brado project.
+//
+// Licensed under the MIT license <LICENSE or https://opensource.org/licenses/MIT>.
+// This file may not be copied, modified, or distributed except according to
+// those terms.
+
+//! Utilitários para validação de documentos brasileiros
 //!
-//! brado is a brazilian docs validator.
+//! Brado fornece funções para identificação, validação e geração de documentos
+//! brasileiros. O nome desta biblioteca, Brado, é um acronimo de BRAzilian
+//! DOcs validator (validador de DOcumentos BRAsileiros).
+//!  
+//! # Guia Rápido
 //!
-//! ## Example
+//! Para utilizar a biblioteca, basta importá-la no arquivo (`use brado`),
+//! escolher o módulo correspondente ao documento e chamar a função desejada.
+//! Por exemplo, para validar um CPF, basta utilizar o código a seguir:
+//!
 //! ```
-//! use brado::cpf;
-//! cpf::validate("639.292.470-11");
+//! use brado;
+//! let result = brado::cpf::validate("639.292.470-11");
+//! assert!(result);
 //! ```
 pub mod cnh;
 pub mod cnpj;
