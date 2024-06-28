a697ece62f5e68dc3dcb15cce8703f77d5f7462a
Breno Viana <brenomfviana@gmail.com>
2024-06-28 08:46:36 -0300

    adiciona identificação de certidões

--- brado/src/docs.rs
+++ brado/src/docs.rs
@@ -1,5 +1,6 @@
 //! Utilitários para validação de documentos.
 
+use crate::certidao;
 use crate::cnh;
 use crate::cnpj;
 use crate::cns;
@@ -223,4 +224,35 @@ pub fn is_eleitoral(doc: &str) -> bool {
 /// ```
 pub fn is_renavam(doc: &str) -> bool {
     renavam::validate(doc)
+}
+
+/// Verifica se um documento `doc` é uma Certidão, máscarado ou
+/// não. Retorna `true` se o argumento `doc` for uma Certidão
+/// válido, caso contrário, retorna `false`.
+///
+/// ## Exemplos
+///
+/// Certidões válidos:
+/// ```
+/// use brado::docs;
+///
+/// let result = docs::is_certidao("21924201552023106304243115818536"); // true
+/// assert!(result);
+///
+/// let result = docs::is_certidao("219242 01 55 2023 1 06304 243 1158185-36"); // true
+/// assert!(result);
+/// ```
+///
+/// Certidões inválidos:
+/// ```
+/// use brado::docs;
+///
+/// let result = docs::is_certidao("21924201552023106304243115818535"); // false
+/// assert!(!result);
+///
+/// let result = docs::is_certidao("219242 01 55 2023 1 06304 243 1158185-35"); // false
+/// assert!(!result);
+/// ```
+pub fn is_certidao(doc: &str) -> bool {
+    certidao::validate(doc)
 }
